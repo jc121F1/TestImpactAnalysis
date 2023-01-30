9bbd2077f392a54c2ef99b6237fa529f93f2aa03
Jack Curtis <jackcurtis121@gmail.com>
2023-01-30 20:53:14 +0000

    Add coverage parser

---
+++ coverage_parser.py
@@ -0,0 +1,29 @@
+import json
+import pathlib
+
+FILE_CONST = "files"
+COV_TEST_CONST = 'covering_tests'
+CONTEXTS = "contexts"
+
+class Parser():
+    __json_obj = {}
+    __files = {}
+
+
+    def __init__(self, filepath: pathlib.Path):
+        with open(filepath, "rb") as f:
+            self.__json_obj: dict = json.load(f)
+        
+        for file_name, file_info in self.__json_obj[FILE_CONST].items():
+            self.__files[file_name] = self.parse_covering_tests(file_info)
+
+    def parse_covering_tests(self, file_info):
+        covering_tests = []
+        for line, contexts in file_info[CONTEXTS].items():
+            covering_tests.extend(contexts)
+        file_info[COV_TEST_CONST] = [x for x in set(covering_tests) if x != ""]
+        return file_info
+    
+    @property
+    def get_file_names(self):
+        return self.__files.keys()
--- launcher.py
+++ launcher.py
@@ -1,10 +1,32 @@
 import argparse
+import coverage_parser
+import pathlib
+
+FROM_COVERAGE = "from_coverage"
+
 
 def parse_args():
+
+    def file(file_path):
+        if pathlib.Path(file_path).is_file():
+            return file_path
+        else:
+            return ValueError("Error, file does not exist.")
+
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--from_coverage",
+                        type=file,
+                        help="Path a valid coverage json file to be used to seed maps",
+                        required=False)
+    return parser.parse_args()
+
+
 def main(args: dict):
-    pass
+    coverage_file = args.get(FROM_COVERAGE)
+    if coverage_file:
+        print(coverage_parser.Parser(coverage_file).get_file_names)
+
 
 if __name__ == "__main__":
     main(vars(parse_args()))
