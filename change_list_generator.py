b2d0e5d6ee37d651bcd4da248397139d2e8fd8dd
Jack Curtis <jackcurtis121@gmail.com>
2023-02-09 16:50:17 +0000

    Add change list generator class

--- change_list_generator.py
+++ change_list_generator.py
@@ -1,28 +1,61 @@
-from git import Repo, Commit, Diff
+from git import Repo, Commit, Diff, GitError
 from pathlib import Path
 
 
-
 class ChangeListGenerator:
-    def __init__(self, path : Path):
+    """
+        This class is used to generate git.Diff objects that represent the changelist between two commits.
+        It takes a repo as an argument upon creation.
+    """
 
+    def __init__(self, path: Path):
+        """
+           Creates a ChangeListGenerator object.
+
+        Args:
+            path (Path): Local path to the git repository that we want to build a changelist in.
+        """
         self.repo = Repo(path)
 
-
     def get_changelist(self, initial_commit_id, final_commit_id):
-        try:
-            init_commit = self.repo.commit(initial_commit_id)
-            print(f"Selecting init_commit as : {init_commit}")
-            final_commit = self.repo.commit(final_commit_id)
-            print(f"Selecting final commit as : {final_commit}")
-        except Exception as e:
-            print("An exception occured, it is as follows:  {e}")
+        """
+            Generates the Diff object between two commits. Initial commit must be the ancestor of the final commit (that is, comes before in our tree of commits)
 
-        diff = final_commit.diff(init_commit)
-        
-        return diff
+        Args:
+            initial_commit_id (str): Commit id for the first commit we want to compare
+            final_commit_id (str): Commit id for the second commit we want to compare.
 
-    
-c = ChangeListGenerator(Path("C:\\Users\\jackc\Desktop\\TestImpactAnalysis"))
+        Raises:
+            ValueError: If initial commit is not an ancestor of final commit, this error will be raised.
 
-print(c.get_changelist("HEAD~1","main"))
+        Returns:
+            git.DiffIndex : The diff between the initial and final commit, containing all changes and their types in an iterable.
+        """
+        if self.verify_initial_commit_is_before_final_commit(initial_commit_id, final_commit_id):
+
+            try:
+                init_commit = self.repo.commit(initial_commit_id)
+                print(f"Selecting init_commit as : {init_commit}")
+                final_commit = self.repo.commit(final_commit_id)
+                print(f"Selecting final commit as : {final_commit}")
+                diff = final_commit.diff(init_commit)
+            except Exception as e:
+                print("An exception occured, it is as follows:  {e}")
+
+            return diff
+        else:
+            raise ValueError(
+                "Initial commit occurs after final commit. This is an invalid configuration for generating a changelist.")
+
+    def initial_commit_is_before_final_commit(self, initial_commit_id, final_commit_id):
+        """
+            Returns whether the commit referred to by initial_commit_id is an ancestor of final_commit_id
+
+        Args:
+            initial_commit_id (str): Commit id for the first commit we want to compare
+            final_commit_id (str): Commit id for the second commit we want to compare.
+
+        Returns:
+            Bool: Is the commit referred to by initial_commit_id is an ancestor of final_commit_id, true or false.
+        """
+        return self.repo.is_ancestor(initial_commit_id, final_commit_id)
