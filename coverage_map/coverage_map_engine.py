2c6a683ede6390d7190a58efabf3090511e4f4da
Jack Curtis <jackcurtis121@gmail.com>
2023-02-13 12:37:24 +0000

    First pass coverage map engine

--- coverage_map/coverage_map_engine.py
+++ coverage_map/coverage_map_engine.py
@@ -1,16 +1,29 @@
 from pathlib import Path
+from coverage_generator import CoverageGenerator as Generator
+from coverage_map_local_storage import LocalCoverageMapStorage as LocalStorage
+from coverage_map_storage import StorageMode as SM, RetentionPolicy as RP
+from coverage_map_logger import get_logger
+
+logger = get_logger(__file__)
 
 
 class CoverageMapEngine():
 
-    def __init__(self, coverage_dir: Path):
+    def __init__(self, coverage_dir: Path, storage_mode: SM, storage_retention_policy: RP, pytest_args, coverage_args):
         self.coverage_dir = coverage_dir
-        if ((coverage := self.retrieve_coverage(coverage_dir))):
-            # return coverage map
-            pass
+        self.storage_mode = storage_mode
+        self.storage_retention_policy = storage_retention_policy
+        if (coverage := self.retrieve_coverage(coverage_dir)):
+            self.coverage = coverage
         else:
-            # generate coverage
-            pass
+            generator = Generator(pytest_args, coverage_args)
+            generator.generate_coverage()
 
-    def retrieve_coverage(coverage_dir: str):
-        pass
+    def retrieve_coverage(self, coverage_dir: str):
+        if self.storage_mode == SM.LOCAL:
+            if (storage := LocalStorage(coverage_dir, self.storage_retention_policy).has_map):
+                return storage.map
+        return None
+
+
+cm = CoverageMapEngine(Path("test"), SM.LOCAL, RP.KEEP_ALL, "py", "cov")
