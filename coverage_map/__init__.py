8a3bbfd3ae28d1730e4b4ca20ca044a773f0730c
Jack Curtis <jackcurtis121@gmail.com>
2023-02-13 13:47:34 +0000

    refactor init

--- coverage_map/__init__.py
+++ coverage_map/__init__.py
@@ -1,6 +1,6 @@
-from .coverage_map_engine import CoverageMapEngine as Engine
-from .coverage_map_local_storage import LocalCoverageMapStorage as LocalStorage
-from .coverage_map_storage import StorageMode, RetentionPolicy
-from .coverage_parser import CoverageParserAndProcessor as ParserAndProcessor
-from .coverage_generator import CoverageGenerator as Generator
-from .coverage_map_logger import get_logger
+from .coverage_map_engine import *
+from .coverage_map_local_storage import *
+from .coverage_map_storage import *
+from .coverage_parser import *
+from .coverage_generator import *
+from .coverage_map_logger import *
