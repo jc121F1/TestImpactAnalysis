4c67e890d8e64a84b5a540480fc265986a27eb07
Jack Curtis <jackcurtis121@gmail.com>
2023-02-13 15:25:31 +0000

    Format

--- test_selection/test_selection_engine.py
+++ test_selection/test_selection_engine.py
@@ -4,11 +4,14 @@ from pathlib import Path
 from .test_selection_logger import get_logger
 
 logger = get_logger(__file__)
+
+
 class TestSelectionEngine:
 
     def __init__(self, changelist: DiffIndex, test_selection_mode, coverage_dir: Path):
         self.changelist = changelist
-        self.coverage_map_engine = coverage_map.CoverageMapEngine(coverage_dir, coverage_map.StorageMode.LOCAL, coverage_map.RetentionPolicy.KEEP_ALL, "", "")
+        self.coverage_map_engine = coverage_map.CoverageMapEngine(
+            coverage_dir, coverage_map.StorageMode.LOCAL, coverage_map.RetentionPolicy.KEEP_ALL, "", "")
         self.test_selection_mode = test_selection_mode
 
     def select_tests(self):
@@ -18,11 +21,13 @@ class TestSelectionEngine:
                 try:
                     tests_to_execute += self.coverage_map_engine.coverage_map[changed_file.a_path]
                 except KeyError as e:
-                    logger.warning(f"File {changed_file.a_path} was changed but we have no coverage data available for this file. Executing all tests.")
+                    logger.warning(
+                        f"File {changed_file.a_path} was changed but we have no coverage data available for this file. Executing all tests.")
                     return []
-            
+
             return set(tests_to_execute)
 
         else:
-            logger.warning("No coverage data available. All tests will be executed.")
+            logger.warning(
+                "No coverage data available. All tests will be executed.")
             return []
