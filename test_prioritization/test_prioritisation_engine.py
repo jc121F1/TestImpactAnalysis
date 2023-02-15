d220060e06efc917bbb520b25f889e23796e22c3
Jack Curtis <jackcurtis121@gmail.com>
2023-02-15 18:57:38 +0000

    Add alphabetical prioritisation

--- test_prioritization/test_prioritisation_engine.py
+++ test_prioritization/test_prioritisation_engine.py
@@ -1,4 +1,20 @@
+from enum import Enum
+import pytest
+
+
+class TestPrioritisationPolicy(Enum):
+    ALPHABETICAL = 1
+    BY_CHANGE_PERCENTAGE = 2
+
+
 class TestPrioritisationEngine():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, test_prioritisation_policy: TestPrioritisationPolicy) -> None:
+        self.prioritisation_policy = test_prioritisation_policy
+
+    def prioritise_tests(self, selected_tests: list):
+        if self.prioritisation_policy == TestPrioritisationPolicy.ALPHABETICAL:
+            return sorted(selected_tests)
+        elif self.prioritisation_policy == TestPrioritisationPolicy.BY_CHANGE_PERCENTAGE:
+            #TODO: Implement by change percentage support
+            pass
