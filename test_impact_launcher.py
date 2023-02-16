41b3c007ac57ba51d04236853123549af6a3f2ab
Jack Curtis <jackcurtis121@gmail.com>
2023-02-16 22:33:38 +0000

    Add more parser arguments and feed through to required location

--- test_impact_launcher.py
+++ test_impact_launcher.py
@@ -1,22 +1,26 @@
 import argparse
-import coverage_map
-import test_selection
+from coverage_map.coverage_map_storage import StorageMode, RetentionPolicy
+from test_selection import TestSelectionEngine, TestSelectionPolicy
+from change_list_generator import ChangeListGenerator
+from pathlib import Path
 import test_prioritization
-import change_list_generator
 import pathlib
 import pprint
 
 FROM_COVERAGE = "from_coverage"
 TEST_RUNNER_ARGS = "test_runner_args"
 COVERAGE_ARGS = "coverage_args"
-INIT_COMMIT= "init_commit"
+INIT_COMMIT = "init_commit"
 FINAL_COMMIT = "final_commit"
+TEST_SELECTION_POLICY = "test_selection_policy"
+STORAGE_MODE = "storage_mode"
+RETENTION_POLICY = "storage_retention_policy"
 
 
 def parse_args():
 
     def file(file_path):
-        if pathlib.Path(file_path).is_file():
+        if Path(file_path).is_file():
             return file_path
         else:
             return ValueError("Error, file does not exist.")
@@ -37,7 +41,7 @@ def parse_args():
                         type=str,
                         help="Args to apply to coverage module if necessary.",
                         required=False)
-    
+
     parser.add_argument("--init-commit",
                         type=str,
                         help="Initial commit that we will generate changes from.",
@@ -46,32 +50,46 @@ def parse_args():
                         type=str,
                         help="Final commit which we will generate changes from",
                         required=False)
+    parser.add_argument("--test-selection-policy",
+                        type=TestSelectionPolicy,
+                        help="Test selection policy: covering_tests | covering_tests_and_dependencies | all. Defaults to covering_tests",
+                        required=False)
+
+    parser.add_argument("--storage-mode",
+                        type=StorageMode,
+                        help="Storage mode for test impact files: local  Defaults to local",
+                        required=False)
+
+    parser.add_argument("--storage-retention-policy",
+                        type=RetentionPolicy,
+                        help="Retention policy for test impact files: keep_all | keep_ten | keep_one.  Defaults to keep_all",
+                        required=False)
+
     return parser.parse_args()
 
 
 def main(args: dict):
-    conv = lambda i: i or ""
-    args = {key: conv(value) for (key, value) in args.items()}
-    coverage_file = args.get(FROM_COVERAGE)
-    test_runner_args = args.get(TEST_RUNNER_ARGS)
-    coverage_args = args.get(COVERAGE_ARGS)
-    init_commit = args.get(INIT_COMMIT)
-    final_commit = args.get(FINAL_COMMIT)
 
-    # cm = coverage_map.CoverageMapEngine(pathlib.Path(
-    #   "test_dir"), coverage_map.StorageMode.LOCAL, coverage_map.RetentionPolicy.KEEP_ALL, "", "")
+    test_selection_policy = args.pop(
+        TEST_SELECTION_POLICY) or TestSelectionPolicy.SELECT_COVERING_TESTS
+    storage_mode = args.pop(STORAGE_MODE) or StorageMode.LOCAL
+    retention_policy = args.pop(RETENTION_POLICY) or RetentionPolicy.KEEP_ALL
+    coverage_file = args.get(FROM_COVERAGE) or ""
+    test_runner_args = args.get(TEST_RUNNER_ARGS) or ""
+    coverage_args = args.get(COVERAGE_ARGS) or ""
+    init_commit = args.get(INIT_COMMIT) or ""
+    final_commit = args.get(FINAL_COMMIT) or ""
 
-    cg = change_list_generator.ChangeListGenerator(pathlib.Path.cwd())
-    changelist = cg.get_changelist(
-        init_commit, final_commit)
-    te = test_selection.TestSelectionEngine(changelist, test_selection.TestSelectionPolicy.SELECT_COVERING_TESTS, pathlib.Path(
-        "coverage_dir"), test_runner_args, coverage_args)
+    cg = ChangeListGenerator(pathlib.Path.cwd())
+    changelist = cg.get_changelist(init_commit, final_commit)
+    te = TestSelectionEngine(changelist, test_selection_policy, Path(
+        "coverage_dir"), test_runner_args, coverage_args, storage_mode, retention_policy)
     selected_tests = te.select_tests()
-    te.store_coverage_map()
     pe = test_prioritization.TestPrioritisationEngine(
         test_prioritization.TestPrioritisationPolicy.ALPHABETICAL)
     prioritised_list = pe.prioritise_tests(selected_tests)
     pprint.pprint(prioritised_list)
+    te.store_coverage_map()
 
 
 if __name__ == "__main__":
