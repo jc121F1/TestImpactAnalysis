c58454fcab500cbf6cda2e4446c4bb9438a8994a
jc121f1 <jackcurtis121@gmail.com>
2023-02-24 12:18:01 +0000

    Add source dir arg

--- test_impact_launcher.py
+++ test_impact_launcher.py
@@ -20,11 +20,20 @@ TEST_SELECTION_POLICY = "test_selection_policy"
 STORAGE_MODE = "storage_mode"
 RETENTION_POLICY = "storage_retention_policy"
 COVERAGE_TARGET = "coverage_target"
+SOURCE_DIR = "source_directory"
 
 logger = get_logger(__file__)
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
+
+    def isfolder(value):
+        if pathlib.Path(value).is_dir():
+            return True
+        else:
+            raise ValueError(
+                f"Error, given argument {value} is not a directory.")
 
     parser.add_argument("--test-runner-args",
                         type=str,
@@ -44,6 +53,7 @@ def parse_args():
                         type=str,
                         help="Final commit which we will generate changes from",
                         required=False)
+
     parser.add_argument("--test-selection-policy",
                         type=TestSelectionPolicy,
                         help="Test selection policy: covering_tests | covering_tests_and_dependencies | all. Defaults to covering_tests",
@@ -64,13 +74,18 @@ def parse_args():
                         help="Folder to store test_impact files in. Does not need to exist. Defaults to 'current_working_dir/coverage_dir'",
                         required=False)
 
+    parser.add_argument("--source-directory",
+                        type=isfolder,
+                        help="Folder that contains source files to scan for dependencies when using the \"select covering and dependencies\" test selection policy",
+                        required=False)
+
     return parser.parse_args()
 
 
 def main(args: dict):
 
     test_selection_policy = args.pop(
-        TEST_SELECTION_POLICY) or TestSelectionPolicy.SELECT_COVERING_TESTS
+        TEST_SELECTION_POLICY) or TestSelectionPolicy.SELECT_COVERING_AND_DEPENDENCIES
     storage_mode = args.pop(STORAGE_MODE) or StorageMode.LOCAL
     retention_policy = args.pop(RETENTION_POLICY) or RetentionPolicy.KEEP_ALL
     coverage_file = args.get(COVERAGE_TARGET) or Path("coverage_dir")
@@ -78,6 +93,7 @@ def main(args: dict):
     coverage_args = args.get(COVERAGE_ARGS) or ""
     init_commit = args.get(INIT_COMMIT) or ""
     final_commit = args.get(FINAL_COMMIT) or ""
+    source_directory = args.get(SOURCE_DIR) or ""
 
     cg = GitChangeListGenerator(Path.cwd())
     changelist = cg.get_changelist(init_commit, final_commit)
@@ -90,7 +106,8 @@ def main(args: dict):
     test_info = PyTestTestInformationExtractor().load_test_information()
 
     te = TestSelectionEngine(test_selection_policy)
-    selected_tests = te.select_tests(changelist, coverage_map, test_info)
+    selected_tests = te.select_tests(
+        changelist, coverage_map, test_info, source_directory)
     selected_tests_string = '\n'.join(selected_tests)
     logger.info(
         f"The following tests have been selected by Test Impact Analysis:")
@@ -99,10 +116,11 @@ def main(args: dict):
 
     pe = TestPrioritisationEngine(
         TestPrioritisationPolicy.ALPHABETICAL)
-    
+
     prioritised_list = pe.prioritise_tests(selected_tests)
     tr = PytestTestRunnerEngine()
-    additive_coverage_map, return_code = tr.execute_tests("", "", prioritised_list, test_info)
+    additive_coverage_map, return_code = tr.execute_tests(
+        "", "", prioritised_list, test_info)
     logger.info(f"Test execution concluded with returncode {return_code}")
     coverage_map.update(additive_coverage_map)
     coverage_map_engine.store_coverage(coverage_map)
