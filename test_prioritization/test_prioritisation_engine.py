5c75d634c9a0bcb162746af51a82a44df71e06d5
Jack Curtis <jackcurtis121@gmail.com>
2023-03-02 16:27:45 +0000

    Add comments

--- test_prioritization/test_prioritisation_engine.py
+++ test_prioritization/test_prioritisation_engine.py
@@ -8,13 +8,13 @@ class TestPrioritisationPolicy(Enum):
 
 
 class TestPrioritisationEngine():
+    """
+    Class responsible for sorting selected tests into an order for execution.
+    """
 
-    def __init__(self, test_prioritisation_policy: TestPrioritisationPolicy) -> None:
+    def __init__(self, test_prioritisation_policy: TestPrioritisationPolicy):
         self.prioritisation_policy = test_prioritisation_policy
 
     def prioritise_tests(self, selected_tests: list):
         if self.prioritisation_policy == TestPrioritisationPolicy.ALPHABETICAL:
             return sorted(selected_tests)
-        elif self.prioritisation_policy == TestPrioritisationPolicy.BY_CHANGE_PERCENTAGE:
-            #TODO: Implement by change percentage support
-            pass
